a1b8eb38f1a1d9a31c3a4b5e91084b98b60bcb82
Andrew Selbie <aselbie@gmail.com>
2016-06-19 13:09:46 -0700

    Add tests and handle error cases

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Person struct {
@@ -15,29 +15,54 @@ type Response struct {
 	Body string `json:"body"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func sayHiHandler(w http.ResponseWriter, r *http.Request) {
 	var person Person
 
+	// Reject non-POST requests to this endpoint
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Reject request if message body is empty
 	if r.Body == nil {
-		http.Error(w, "Please send a request body", 400)
+		http.Error(w, "Please send a request body", http.StatusBadRequest)
 		return
 	}
 
 	err := json.NewDecoder(r.Body).Decode(&person)
 
+	// Reject request if JSON parsing fails
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	fmt.Println(person)
+	// Build up missingFields slice
+	missingFields := []string{}
+
+	if person.FirstName == "" {
+		missingFields = append(missingFields, "firstName")
+	}
+
+	if person.LastName == "" {
+		missingFields = append(missingFields, "lastName")
+	}
+
+	// Reject request if required fields are missing
+	if len(missingFields) > 0 {
+		http.Error(w, "The following fields were missing from your request: " + strings.Join(missingFields[:],", "), http.StatusBadRequest)
+		return
+	}
 
 	response := Response{"Hi " + person.FirstName + " " + person.LastName}
 
 	json.NewEncoder(w).Encode(response)
 }
 
+// Kickoff server
+// Default behavior returns 404 for unhandled routes
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/sayhi", sayHiHandler)
 	http.ListenAndServe(":3000", nil)
 }
---
+++ main_test.go
@@ -0,0 +1,67 @@
+package main
+
+import(  
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "bytes"
+)
+
+var fullJsonString = []byte(`{"firstName": "Jon", "lastName": "Snow"}`)
+var missingJsonString = []byte(`{"firstName": "Jon"}`)
+var invalidJsonString = []byte(`{"firstName": "Jon", "last`)
+
+func TestValidRequest(t *testing.T) {
+    req, _ := http.NewRequest("POST", "", bytes.NewBuffer(fullJsonString))
+    w := httptest.NewRecorder()
+
+    sayHiHandler(w, req)
+
+    if w.Code != http.StatusOK {
+        t.Errorf("A valid request should return %v", http.StatusOK)
+    }
+}
+
+func TestInvalidMethod(t *testing.T) {
+    req, _ := http.NewRequest("GET", "", bytes.NewBuffer(fullJsonString))
+    w := httptest.NewRecorder()
+
+    sayHiHandler(w, req)
+
+    if w.Code != http.StatusMethodNotAllowed {
+        t.Errorf("Methods other than POST should return %v", http.StatusMethodNotAllowed)
+    }
+}
+
+func TestNoBody(t *testing.T) {
+    req, _ := http.NewRequest("POST", "", nil)
+    w := httptest.NewRecorder()
+
+    sayHiHandler(w, req)
+
+    if w.Code != http.StatusBadRequest {
+        t.Errorf("Requests without a body should return %v", http.StatusBadRequest)
+    }
+}
+
+func TestMissingField(t *testing.T) {
+    req, _ := http.NewRequest("POST", "", bytes.NewBuffer(missingJsonString))
+    w := httptest.NewRecorder()
+
+    sayHiHandler(w, req)
+
+    if w.Code != http.StatusBadRequest {
+        t.Errorf("Requests with missing fields should return %v", http.StatusBadRequest)
+    }
+}
+
+func TestInvalidJson(t *testing.T) {
+    req, _ := http.NewRequest("POST", "", bytes.NewBuffer(invalidJsonString))
+    w := httptest.NewRecorder()
+
+    sayHiHandler(w, req)
+
+    if w.Code != http.StatusBadRequest {
+        t.Errorf("Requests without a body should return %v", http.StatusBadRequest)
+    }
+}
